a9f7ff05f3053b350aaeceaee39387ec6c4f9fbc
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2016-01-06 23:35:27 +0200

    Added gulp for less. Initial 1st and 2nd section.

---
+++ src/YouthTechclub.Web/gulpfile.js
@@ -0,0 +1,18 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+// Compiles LESS > CSS 
+gulp.task('build-less', function () {
+    return gulp.src('./wwwroot/less/styles.less')
+        .pipe(less())
+        .pipe(gulp.dest('./wwwroot/css'));
+});

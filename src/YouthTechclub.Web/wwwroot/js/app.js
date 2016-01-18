37a8d409c16e3ab91693ba4e9c952673652bce4d
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2016-01-18 22:05:22 +0200

    Contact form responsivness started

---
+++ src/YouthTechclub.Web/wwwroot/js/app.js
@@ -0,0 +1,42 @@
+﻿var previousSize = "big";
+
+$(document).ready(function () {
+
+    if ($(".contact__container").css("width") == "80%") {
+        previousSize = "small";
+    } else {
+        previousSize = "big";
+    }
+
+    $(window).resize(checkSize);
+});
+
+function checkSize() {
+
+    if ($(".contact__container").css("width") == "80%") {
+        
+        //Swap description with input field if previous size is big
+        if (previousSize == "big") {
+            var description = $(".form_description");
+            var email = $(".form_email");
+
+            $(".form_description").remove();
+            $(".form_email").remove();
+
+            $(".form_name").after(description);
+            $(".form_name").after(email);
+        }
+    } else {
+        //Swap them back
+        if (previousSize == "small") {
+            var description = $(".form_description");
+            var email = $(".form_email");
+
+            $(".form_description").remove();
+            $(".form_email").remove();
+
+            $(".form_name").after(email);
+            $(".form_name").after(description);
+        }
+    }
+}

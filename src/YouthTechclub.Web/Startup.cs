edf0fd7497127944affb719aadcd81ec36b90972
KRUROL <rolands.krumbergs@if.lv>
2016-02-28 12:56:52 +0200

    Updating index html routing

--- src/YouthTechclub.Web/Startup.cs
+++ src/YouthTechclub.Web/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace YouthTechclub.Web
